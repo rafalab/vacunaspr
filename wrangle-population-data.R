1ae414e7dd84b5711e93d73462f66910d801cb3a
Rafael A. Irizarry <rairizarry@gmail.com>
2022-02-04 14:40:46 -0500

    adding new (2020) age specific population sizes

--- wrangle-population-data.R
+++ wrangle-population-data.R
@@ -3,6 +3,9 @@ library(lubridate)
 library(data.table)
 
 pr_pop <- 3285874 ## population of puerto rico
+pr_adult_pop <- 2724903
+pr_child_pop <- pr_pop - pr_adult_pop
+
 
 if(Sys.info()["nodename"] == "fermat.dfci.harvard.edu"){
   rda_path <- "/homes10/rafa/dashboard/vacunaspr/rdas"
@@ -15,27 +18,20 @@ age_starts <- as.numeric(str_extract(age_levels, "\\d+"))
 breaks <- sort(age_starts)
 labels <- c(paste(breaks[-length(breaks)], c(breaks[-1]-1), sep="-"),
             paste0(breaks[length(breaks)], "+"))
+## downlaoded data like this: curl https://www2.census.gov/programs-surveys/popest/datasets/2010-2020/state/asrh/PRC-EST2020-SYASEX.csv >> PRC-EST2020-SYASEX.csv
 pop_by_age_gender  <-
-    readxl::read_xlsx("data/prc-est2019-syasex.xlsx",  
-                               skip = 4) %>%
-  select("...1", "Male...36", "Female...37")  %>% 
-  setNames(c("ageRange", "M", "F")) %>%
-  mutate(ageRange= str_remove(ageRange, "."))  %>% 
-  filter(str_detect(ageRange, "^\\d")) %>% 
-  mutate(ageRange = str_remove(ageRange,"\\+")) %>%
-  mutate(ageRange = as.numeric(ageRange)) %>% 
-  pivot_longer(-ageRange, names_to="gender", values_to = "poblacion") 
-
-pr_adult_pop <- pop_by_age_gender %>% filter(ageRange>= 18) %>%
-  summarize(total = sum(poblacion)) %>%
-  pull(total)
-old_pop <- sum(pop_by_age_gender$poblacion)
-
-pop_by_age_gender <- pop_by_age_gender %>%
-  mutate(poblacion = poblacion*pr_pop/old_pop) %>% 
+  read_csv("data/PRC-EST2020-SYASEX.csv") %>%
+  select(SEX, AGE, POPESTIMATE2020) %>%
+  setNames(c("gender", "ageRange", "poblacion")) %>%
+  filter(gender !=0 & ageRange != 999) %>%
+  mutate(gender = ifelse(gender==1, "M", "F")) %>%
+  mutate(g = ifelse(ageRange>= 18, "adult", "child")) %>%
+  left_join(data.frame(g = c("adult", "child"), new = c(pr_adult_pop, pr_child_pop)), by = "g") %>%
+  group_by(g) %>%
+  mutate(poblacion = poblacion/sum(poblacion)*new) %>%
+  ungroup() %>%
   mutate(ageRange = factor(as.character(cut(ageRange, c(breaks, Inf), right = FALSE,
                                             labels = labels)), levels = labels)) %>%
-  filter(!is.na(ageRange)) %>%
   group_by(ageRange, gender) %>%
   summarize(poblacion = sum(poblacion), .groups = "drop") %>%
   as.data.table() 
